e8cd26b0c0411e471974a312e0f28b5c2870e1c6
Jin Kwon <onacit@gmail.com>
2020-01-05 14:51:19 +0900

    Add XML Schema files

---
+++ src/main/java/com/github/jinahya/doi/metadata/kernel/bind/App.java
@@ -0,0 +1,13 @@
+package com.github.jinahya.doi.metadata.kernel.bind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/github/jinahya/doi/metadata/kernel/bind/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.jinahya.doi.metadata.kernel.bind;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
